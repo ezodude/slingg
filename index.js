ca2fa070e0fcd70ac57c29c860734549f8fcd50e
Ezo Saleh <ezo@the-experimenters.com>
2016-02-03 11:18:28 +0000

    Mapping columns complete. Includes correct parsing of different inputs.

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
 #! /usr/bin/env node
 
-var _           = require('highland')
+var h           = require('highland')
+  , _      = require('lodash')
   , program     = require('commander')
   , XLSX        = require('xlsx')
   , request     = require('superagent')
   , through2    = require('through2')
-  , url;
+  , mapping;
 
 var exceltoJSONStream = function(file){
   var workbook = XLSX.readFile(file);
   var ws = workbook.Sheets[workbook.SheetNames[0]];
   var json = XLSX.utils.sheet_to_json(ws);
-  return _(json);
+  return h(json);
 };
 
 var lowerCasedKeys = function(row){
-  _.keys(row).each(function(key){
+  h.keys(row).each(function(key){
     row[key.toLowerCase()] = row[key];
     delete row[key];
   });
   return row;
 };
 
+var mapColumns = function(row){
+  var invertedMapping = _.invert(mapping);
+  h.values(mapping).each(function(val){
+    row[invertedMapping[val]] = row[val];
+    delete row[val];
+  });
+  return row;
+};
+
+function parseMapCols(val){
+  var result = {};
+  var val = val.replace(/\s|\"|\{|\}/g, '');
+  val.split(',').map(_.trim).forEach(function(e){
+    var parts = e.split(':');
+    result[parts[0]] = parts[1] === 'null' ? null : parts[1];
+  });
+  return result;
+}
+
 program
 .usage('[options] <file>')
 .arguments('<file>')
 .option('-u, --url <http request target>', 'URL to work with.')
-.option('-m, --mapping [override1:original1, ...]', 'Optional column mappings to override originals in xls/csv.')
+.option('-c, --map-cols [override1:original1, ...]', 'Optional column headers to override originals in xls/csv.', parseMapCols)
 .action(function(file) {
+  mapping = program.mapping;
+
   exceltoJSONStream(file)
   .map(lowerCasedKeys)
-  .pipe(through2.obj(function(buf, _, next){
+  .map(mapColumns)
+  .map(JSON.stringify)
+  .pipe(through2.obj(function(buf, h, next){
     request.post(program.url).send(buf).end(function(err, res){
       next(err, JSON.stringify(res));
     });
